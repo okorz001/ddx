5075e747ea21e81697fa4ee8fcc72b528a0e4a7a
Oscar Korz <okorz001@gmail.com>
2012-03-17 12:17:19 -0700

    Use Token type during parsing
    Also replaced list concatenations with simple appending and reverse at
    the end.

--- ddx.hs
+++ ddx.hs
@@ -2,7 +2,9 @@ module Ddx where
 
 import Data.Char
 
-tokenize str = tokenize' str []
+data Token = Name String | Value Double | Add | Sub | Mult | Div deriving(Show)
+
+tokenize str = reverse $ tokenize' str []
 
 -- Identify the start of new tokens. If it's a simple token (i.e. single
 -- character) we can add it directly to the token list.
@@ -11,14 +13,18 @@ tokenize' (x:xs) toks
     | isSpace x  = tokenize' xs toks
     | isLetter x = tokenizeName xs toks [x]
     | isDigit x  = tokenizeNum xs toks [x]
-    | otherwise  = tokenize' xs $ toks ++ [[x]]
+    | x == '+'   = tokenize' xs $ Add:toks
+    | x == '-'   = tokenize' xs $ Sub:toks
+    | x == '*'   = tokenize' xs $ Mult:toks
+    | x == '/'   = tokenize' xs $ Div:toks
+    | otherwise  = error $ "Unrecognized symbol: " ++ [x]
 
-tokenizeName [] toks word = toks ++ [word]
+tokenizeName [] toks word = Name(reverse word):toks
 tokenizeName (x:xs) toks word
-    | isLetter x = tokenizeName xs toks $ word ++ [x]
-    | otherwise  = tokenize' (x:xs) $ toks ++ [word]
+    | isLetter x = tokenizeName xs toks $ x:word
+    | otherwise  = tokenize' (x:xs) $ Name(reverse word):toks
 
-tokenizeNum [] toks word = toks ++ [word]
+tokenizeNum [] toks word = Value(read . reverse $ word):toks
 tokenizeNum (x:xs) toks word
-    | isDigit x = tokenizeNum xs toks $ word ++ [x]
-    | otherwise = tokenize' (x:xs) $ toks ++ [word]
+    | isDigit x = tokenizeNum xs toks $ x:word
+    | otherwise = tokenize' (x:xs) $ Value(read . reverse $ word):toks
