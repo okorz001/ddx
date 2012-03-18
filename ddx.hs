5d0304197a4efbd77ab1ca3053ef0606cf1459e3
Oscar Korz <okorz001@gmail.com>
2012-03-18 10:55:01 -0700

    Add derive function
    Need parse function to go from tokenize to derive. Note that derive does
    not make any attempt to condense or reduce the output expression.

--- ddx.hs
+++ ddx.hs
@@ -28,3 +28,30 @@ tokenizeNum [] toks word = Value(read . reverse $ word) : toks
 tokenizeNum (x : xs) toks word
     | isDigit x = tokenizeNum xs toks $ x : word
     | otherwise = tokenize' (x : xs) $ Value(read . reverse $ word) : toks
+
+data Expr = Var String | Const Double | Sum Expr Expr | Diff Expr Expr |
+        Prod Expr Expr | Quo Expr Expr deriving(Show)
+
+-- TODO: parse :: [Token] -> Expr
+
+derive _ (Const _) = Const 0
+-- Cannot use pattern matching to find equal parameters.
+derive x (Var y) = if x == y then Const 1 else Var y
+derive x (Sum a b) =
+    let a' = derive x a
+        b' = derive x b
+    in  Sum a' b'
+derive x (Diff a b) =
+    let a' = derive x a
+        b' = derive x b
+    in  Diff a' b'
+derive x (Prod a b) =
+    let a' = derive x a
+        b' = derive x b
+    in  Sum (Prod a' b) (Prod a b')
+derive x (Quo a b) =
+    let a' = derive x a
+        b' = derive x b
+        top = Diff (Prod a' b) (Prod a b')
+        bot = Prod b b
+    in  Quo top bot
