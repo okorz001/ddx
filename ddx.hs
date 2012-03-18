76f6d264bb0d2aa77f6879bff80e73a57e4bd3b7
Oscar Korz <okorz001@gmail.com>
2012-03-18 11:36:41 -0700

    Add reduce function

--- ddx.hs
+++ ddx.hs
@@ -30,7 +30,7 @@ tokenizeValue (x : xs) toks word
     | otherwise = tokenize' (x : xs) $ Value(read . reverse $ word) : toks
 
 data Expr = Var String | Const Double | Sum Expr Expr | Diff Expr Expr |
-        Prod Expr Expr | Quo Expr Expr deriving(Show)
+        Prod Expr Expr | Quo Expr Expr deriving(Show, Eq)
 
 -- TODO: parse :: [Token] -> Expr
 
@@ -55,3 +55,46 @@ derive x (Quo a b) =
         top = Diff (Prod a' b) (Prod a b')
         bot = Prod b b
     in  Quo top bot
+
+reduce (Sum e (Const 0)) = reduce e
+reduce (Sum (Const 0) e) = reduce e
+reduce (Sum (Const a) (Const b)) = Const $ a + b
+reduce (Sum a b) =
+    let a' = reduce a
+        b' = reduce b
+        e = Sum a' b'
+    -- Only reduce again if we successfully reduced one of the terms.
+    -- Otherwise we'll get stuck in an infinite loop of failed reductions.
+    in if a' /= a || b' /= b then reduce e else e
+reduce (Diff e (Const 0)) = reduce e
+reduce (Diff (Const a) (Const b)) = Const $ a - b
+reduce (Diff a b) =
+    let a' = reduce a
+        b' = reduce b
+        e = Diff a' b'
+    -- Only reduce again if we successfully reduced one of the terms.
+    -- Otherwise we'll get stuck in an infinite loop of failed reductions.
+    in if a' /= a || b' /= b then reduce e else e
+reduce (Prod e (Const 0)) = Const 0
+reduce (Prod (Const 0) e) = Const 0
+reduce (Prod e (Const 1)) = reduce e
+reduce (Prod (Const 1) e) = reduce e
+reduce (Prod (Const a) (Const b)) = Const $ a * b
+reduce (Prod a b) =
+    let a' = reduce a
+        b' = reduce b
+        e = Prod a' b'
+    -- Only reduce again if we successfully reduced one of the terms.
+    -- Otherwise we'll get stuck in an infinite loop of failed reductions.
+    in if a' /= a || b' /= b then reduce e else e
+reduce (Quo (Const 0) e) = Const 0
+reduce (Quo e (Const 1)) = reduce e
+reduce (Quo (Const a) (Const b)) = Const $ a / b
+reduce (Quo a b) =
+    let a' = reduce a
+        b' = reduce b
+        e = Quo a' b'
+    -- Only reduce again if we successfully reduced one of the terms.
+    -- Otherwise we'll get stuck in an infinite loop of failed reductions.
+    in if a' /= a || b' /= b then reduce e else e
+reduce e = e
