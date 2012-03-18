6d1ed61dcd108181ceab63792c5e8c5ea9473249
Oscar Korz <okorz001@gmail.com>
2012-03-18 09:43:17 -0700

    Formatting changes
    All binary operators should be surrounded by whitespaces, including :

--- ddx.hs
+++ ddx.hs
@@ -9,22 +9,22 @@ tokenize str = reverse $ tokenize' str []
 -- Identify the start of new tokens. If it's a simple token (i.e. single
 -- character) we can add it directly to the token list.
 tokenize' [] toks = toks
-tokenize' (x:xs) toks
+tokenize' (x : xs) toks
     | isSpace x  = tokenize' xs toks
     | isLetter x = tokenizeName xs toks [x]
     | isDigit x  = tokenizeNum xs toks [x]
-    | x == '+'   = tokenize' xs $ Add:toks
-    | x == '-'   = tokenize' xs $ Sub:toks
-    | x == '*'   = tokenize' xs $ Mult:toks
-    | x == '/'   = tokenize' xs $ Div:toks
+    | x == '+'   = tokenize' xs $ Add : toks
+    | x == '-'   = tokenize' xs $ Sub : toks
+    | x == '*'   = tokenize' xs $ Mult : toks
+    | x == '/'   = tokenize' xs $ Div : toks
     | otherwise  = error $ "Unrecognized symbol: " ++ [x]
 
-tokenizeName [] toks word = Name(reverse word):toks
-tokenizeName (x:xs) toks word
-    | isLetter x = tokenizeName xs toks $ x:word
-    | otherwise  = tokenize' (x:xs) $ Name(reverse word):toks
+tokenizeName [] toks word = Name(reverse word) : toks
+tokenizeName (x : xs) toks word
+    | isLetter x = tokenizeName xs toks $ x : word
+    | otherwise  = tokenize' (x : xs) $ Name(reverse word) : toks
 
-tokenizeNum [] toks word = Value(read . reverse $ word):toks
-tokenizeNum (x:xs) toks word
-    | isDigit x = tokenizeNum xs toks $ x:word
-    | otherwise = tokenize' (x:xs) $ Value(read . reverse $ word):toks
+tokenizeNum [] toks word = Value(read . reverse $ word) : toks
+tokenizeNum (x : xs) toks word
+    | isDigit x = tokenizeNum xs toks $ x : word
+    | otherwise = tokenize' (x : xs) $ Value(read . reverse $ word) : toks
