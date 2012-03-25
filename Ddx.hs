706be9d95aea6ee11400999ad9523796ce45fae6
Oscar Korz <okorz001@gmail.com>
2012-03-24 19:22:37 -0700

    Add DdxMaybe monad for errors
    Some things need some reworking... The code got uglier, not cleaner!

--- Ddx.hs
+++ Ddx.hs
@@ -1,16 +1,34 @@
-module Ddx (Token(..), Expr(..), tokenize, parse, derive, reduce) where
+module Ddx (DdxMaybe(..), Token(..), Expr(..), tokenize, parse, derive, reduce)
+    where
 
 import Data.Char
 import Data.List
 
+newtype DdxMaybe a = DdxMaybe { getEither :: Either String a }
+    deriving(Show)
+
+-- Convenience function for creating a DdxMaybe from an error string.
+ddxError = DdxMaybe . Left
+
+instance Monad DdxMaybe where
+    return x = DdxMaybe $ Right x
+
+    (DdxMaybe (Right x)) >>= f = f x
+    -- TODO: Can this be expressed with less words?
+    -- This is what I really want, but it fails:
+    --      err >>= _ = err
+    (DdxMaybe (Left err)) >>= _ = DdxMaybe $ Left err
+
+    fail = ddxError
+
 data Token = Name String | Value Double | Add | Sub | Mult | Div
     deriving(Show, Eq)
 
-tokenize str = reverse $ tokenize' str []
+tokenize str = tokenize' str []
 
 -- Identify the start of new tokens. If it's a simple token (i.e. single
 -- character) we can add it directly to the token list.
-tokenize' [] toks = toks
+tokenize' [] toks = return $ reverse toks
 tokenize' (x : xs) toks
     | isSpace x  = tokenize' xs toks
     | isLetter x = tokenizeName xs toks [x]
@@ -19,7 +37,7 @@ tokenize' (x : xs) toks
     | x == '-'   = tokenize' xs $ Sub : toks
     | x == '*'   = tokenize' xs $ Mult : toks
     | x == '/'   = tokenize' xs $ Div : toks
-    | otherwise  = error $ "Unrecognized symbol: " ++ [x]
+    | otherwise  = ddxError $ "Unrecognized symbol: " ++ [x]
 
 pushName word toks = Name(reverse word) : toks
 
@@ -39,30 +57,42 @@ data Expr = Var String | Const Double | Sum Expr Expr | Diff Expr Expr |
         Prod Expr Expr | Quo Expr Expr deriving(Show, Eq)
 
 -- Split a list on the first occurance of a key.
-findAndSplit keys items = findAndSplit' $ span (`notElem` keys) items
-
-findAndSplit' (left, []) = (left, Nothing, [])
-findAndSplit' (left, (key : right)) = (left, Just key, right)
+findAndSplit keys items =
+    case span (`notElem` keys) items of
+        (left, []) -> (left, Nothing, [])
+        (left, (key : right)) -> (left, Just key, right)
 
 -- The earlier tokens are parsed, the looser the binding.
 parse toks = parseSumDiff toks
 
-parseSumDiff toks = parseSumDiff' toks $ findAndSplit [Add, Sub] toks
+parseSumDiff toks =
+    case findAndSplit [Add, Sub] toks of
+        (left, (Just Add), right) -> do
+            left' <- parse left
+            right' <- parse right
+            return $ Sum left' right'
+        (left, (Just Sub), right) -> do
+            left' <- parse left
+            right' <- parse right
+            return $ Diff left' right'
+        _ -> parseProdQuo toks
 
-parseSumDiff' toks (left, (Just Add), right) = Sum (parse left) (parse right)
-parseSumDiff' toks (left, (Just Sub), right) = Diff (parse left) (parse right)
-parseSumDiff' toks (_, Nothing, _) = parseProdQuo toks
-
-parseProdQuo toks = parseProdQuo' toks $ findAndSplit [Mult, Div] toks
-
-parseProdQuo' toks (left, (Just Mult), right) = Prod (parse left) (parse right)
-parseProdQuo' toks (left, (Just Div), right) = Quo (parse left) (parse right)
-parseProdQuo' toks (_, Nothing, _) = parseConstVar toks
+parseProdQuo toks =
+    case findAndSplit [Mult, Div] toks of
+        (left, (Just Mult), right) -> do
+            left' <- parse left
+            right' <- parse right
+            return $ Prod left' right'
+        (left, (Just Div), right) -> do
+            left' <- parse left
+            right' <- parse right
+            return $ Quo left' right'
+        _ -> parseConstVar toks
 
 -- Const and Var are terminal. There should not be any other tokens.
-parseConstVar [Value x] = Const x
-parseConstVar [Name x] = Var x
-parseConstVar toks = error $ "Unexpected token(s): " ++ show toks
+parseConstVar [Value x] = return $ Const x
+parseConstVar [Name x] = return $ Var x
+parseConstVar toks = ddxError $ "Unexpected token(s): " ++ show toks
 
 derive _ (Const _) = Const 0
 -- Cannot use pattern matching to find equal parameters.
--- main.hs
+++ main.hs
@@ -1,7 +1,14 @@
 import Control.Monad
 import Ddx
 
+-- Ugh, this feel dirty.
+tryDerive str = do
+    expr <- tokenize str >>= parse
+    return $ reduce . derive "x" $ expr
+
 main = forever $ do
     putStr "Type an expression: (^C to quit.)\n"
     contents <- getLine
-    print $ reduce . derive "x" . parse . tokenize $ contents
+    case tryDerive contents of
+        (DdxMaybe (Left err)) -> print err
+        (DdxMaybe (Right e)) -> print e
